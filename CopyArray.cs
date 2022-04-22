a735ef86c6e3b7691d6a6435b6afaff68c36925d
Xiaojie Y <44991218+senbonzakuray@users.noreply.github.com>
2022-04-21 22:18:40 -0400

    Add files via upload

---
+++ CopyArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] newArr = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArr[i] = array[i];
+                Console.WriteLine($"The {i}th element of array is {array[i]}");
+                Console.WriteLine($"The {i}th element of copied array is {newArr[i]}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
