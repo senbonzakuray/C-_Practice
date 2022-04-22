48e97d7a0e88fd87f9681ff3ad28e44bef7a815b
Xiaojie Y <44991218+senbonzakuray@users.noreply.github.com>
2022-04-21 22:20:28 -0400

    Add files via upload

---
+++ Pyramid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            {
+                for (int i = 1; i <= size; i++)
+                {
+                    for (int j = 0; j <= size - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j < i * 2; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+
+            }
+        }
+    }
+}
